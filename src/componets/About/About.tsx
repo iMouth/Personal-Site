d0f86f042ed735cfad4f3af845c28fe24419c630
Kelvin Kast <kelvin.8.kast@gmail.com>
2023-03-10 19:18:46 -0700

    About me

--- src/componets/About/About.tsx
+++ src/componets/About/About.tsx
@@ -4,16 +4,21 @@ import "./About.scss";
 const About = () => {
   return (
     <div id="About">
-      <div className="title">About</div>
+      <div className="title">About Me</div>
       <div className="content">
         I'm currently a senior undergraduate looking to improve my software and web development knowledge. I study at
-        the University of Arizona, and I'm majoring in computer science. I am skilled with web development languages and
-        frameworks such as JavaScript, Typescript, React.js, Express.js, Node.js, CSS3, HTML5, and SCSS.
+        the University of Arizona, and I'm majoring in computer science.
         <br />
-        Also, have experience working with object-oriented programming languages such as Python, Java and C, database
+        <br />
+        I am skilled with web development languages and frameworks such as JavaScript, Typescript, React.js, Express.js,
+        Node.js, CSS3, HTML5, and SCSS.
+        <br />
+        <br />
+        Also, I have experience working with object-oriented programming languages such as Python, Java and C, database
         MongoDB, and industry tools such as Webpack, Git, Github, ESLint, and Docker.
         <br />
-        Outside of coding, I also enjoy weightlifting, watching reality competitive shows like Survivor, and playing
+        <br />
+        Outside of coding I also enjoy weightlifting, watching reality TV competitive shows like Survivor, and playing
         social deduction games like Mafia.
       </div>
     </div>
